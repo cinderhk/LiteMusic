c6fa1ad1afb05897fef9fa70a689d76b9d2cd33f
dxy <cinderhk@163.com>
2024-09-26 22:50:28 +0800

    初始化项目

---
+++ LiteMusic/src/main/java/com/dxy/litemusic/LiteMusicApplication.java
@@ -0,0 +1,13 @@
+package com.dxy.litemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteMusicApplication.class, args);
+    }
+
+}
